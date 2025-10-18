7750d75ee05438507444ac596a8b24473d1102df
mohamed mostafa <mohamed.mostafa4211@gmail.com>
2025-10-18 12:58:38 +0300

    wip

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,21 @@ import { useData } from './hooks/useData'
 
 
 function App() {
+  //store imp data
+  const BASE_URL = import.meta.env.VITE_BASE_URL
+  const API_KEY = import.meta.env.VITE_API_KEY
 
 
-  const {data, loading, error} = useData(``)
+  const { data, loading, error } = useData(`${BASE_URL}/games?key=${API_KEY}`)
 
 
   return (
     <>
-     <h1>This is from marwan</h1>
+      {data?.results.map(game =>
+        <div>
+          <h1 className='text-white'>{game.slug}</h1>
+        </div>
+      )}
     </>
   )
 }
