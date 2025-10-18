3977d40d33abdfcf3da0e916eb1f1b63bf7e771f
mohamed mostafa <mohamed.mostafa4211@gmail.com>
2025-10-18 13:30:31 +0300

    wip

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,50 @@
 
+import { useState } from 'react'
 import './App.css'
 import { useApi } from './hooks/useApi'
 import { useData } from './hooks/useData'
 
 
 function App() {
-  const {BASE_URL, API_KEY} = useApi()
-  const { data, loading, error } = useData(`${BASE_URL}/games?key=${API_KEY}`)
-
-
+  const { BASE_URL, API_KEY } = useApi()
+  const [url, setUrl] = useState(`${BASE_URL}/games?key=${API_KEY}`)
+  const { data, loading, error } = useData(url)
+  const { page, setPage } = useState(1)
   return (
     <>
-     <div>
-      {data?.results.map(game =>
+      {data &&
         <div>
-          <h1 className='text-white' key={game.slug}>{game.name}</h1>
-        </div>
-      )}
-    </div>
+          <div>
+            {data?.results.map(game =>
+              <div key={game.slug}>
+                <h1 className='text-white'>{game.name}</h1>
+              </div>
+            )}
+          </div>
 
-    <div>
-      <button>prev</button>
-      
-    </div>
+          <div className='text-white text-4xl'>
+            <button
+              disabled={data.previous === null}
+              onClick={() => {
+                if (data.previous !== null) {
+                  setUrl(data.previous)
+                  setPage(page - 1)
+                }
+              }}>
+              prev</button>
+            <h1>{page}</h1>
+            <button
+              disabled={data.next === null}
+              onClick={() => {
+                if (data.next !== null) {
+                  setUrl(data.next)
+                }
+              }}
+            >Next</button>
+          </div>
+        </div>
+      }
     </>
-   
   )
 }
 
