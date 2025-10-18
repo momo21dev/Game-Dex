a29d14f2081cf37327d7b35cf1add9d2c953117f
Marwan Abdalla <marwan3m2000@gmail.com>
2025-10-18 11:10:29 +0200

    wip: working on new hook

--- src/hooks/fetchGames.js
+++
@@ -1,8 +0,0 @@
-const BASE_URL = import.meta.env.VITE_BASE_URL
-const API_KEY = import.meta.env.VITE_API_KEY
-export default function GamesList() {
-    
-    function fetchGames() {
-
-    }
-}
---
+++ src/hooks/useGames.js
@@ -0,0 +1,9 @@
+const BASE_URL = import.meta.env.VITE_BASE_URL
+const API_KEY = import.meta.env.VITE_API_KEY
+
+export function useGames(url = null) {
+   async function fetchGames() {
+        
+   }     
+
+}
