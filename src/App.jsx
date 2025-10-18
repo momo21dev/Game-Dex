f07432dd1cd9cd7366505f9c0788bf65f8554772
Marwan Abdalla <marwan3m2000@gmail.com>
2025-10-18 12:10:07 +0200

    wip

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,30 @@
 
 import './App.css'
+import { useApi } from './hooks/useApi'
 import { useData } from './hooks/useData'
 
 
 function App() {
-  //store imp data
-  const BASE_URL = import.meta.env.VITE_BASE_URL
-  const API_KEY = import.meta.env.VITE_API_KEY
-
-
+  const {BASE_URL, API_KEY} = useApi()
   const { data, loading, error } = useData(`${BASE_URL}/games?key=${API_KEY}`)
 
 
   return (
     <>
+     <div>
       {data?.results.map(game =>
         <div>
-          <h1 className='text-white'>{game.slug}</h1>
+          <h1 className='text-white' key={game.slug}>{game.name}</h1>
         </div>
       )}
+    </div>
+
+    <div>
+      <button>prev</button>
+      
+    </div>
     </>
+   
   )
 }
 
